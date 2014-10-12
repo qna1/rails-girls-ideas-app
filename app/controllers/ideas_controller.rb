aa8a7d1435d3ee77ee4ff7601bc5bfa6312740be
Yana Dimitrova <qna.dimitrova@gmail.com>
2014-10-12 14:29:40 +0000

    create ideas table

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,8 +1,5 @@
 class IdeasController < ApplicationController
   def index
-    @ideas = [
-    {id: 0, title: 'My fist code'},
-    {id: 1, title: 'My fist code'},
-    ]
+    @ideas = Idea.all 
   end
 end
