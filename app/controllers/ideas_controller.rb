cc417ba924fc7f56ed1e0257ed417c9011fa9359
Yana Dimitrova <qna.dimitrova@gmail.com>
2014-10-12 14:57:20 +0000

    allo adding ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,4 +2,12 @@ class IdeasController < ApplicationController
   def index
     @ideas = Idea.all 
   end
-end
+  def new 
+    @idea=Idea.new
+  end 
+  
+  def create
+    Idea.create(params.require(:idea).permit(:title, :description))
+    redirect_to action: :index
+  end
+end 
